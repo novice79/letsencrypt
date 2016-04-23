4bd4d475f2800b6eee54d627421976557082d3c6
david <david@cninone.com>
2016-04-23 23:32:51 +0800

    lets use 80 port

--- init.sh
+++ init.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 set -e
 set -x
+#--standalone-supported-challenges http-01 to use port 80
+#--standalone-supported-challenges tls-sni-01 to use port 443
 
 if [ $# != 0 ]
 then 
 gc='/bin/get_cert.sh'	
 cat > "$gc" <<-EOCMD
 #!/bin/sh
-/letsencrypt/letsencrypt-auto certonly --standalone --agree-tos $@
+/letsencrypt/letsencrypt-auto certonly --standalone --agree-tos --standalone-supported-challenges http-01 $@
 EOCMD
 chmod +x "$gc"
 #exec "$gc"
