4e23029ec2360bcd1ff14307c3396c125356b664
david <david@cninone.com>
2016-04-23 18:22:59 +0800

    test

--- init.sh
+++ init.sh
@@ -7,6 +7,6 @@ cat > "$gc" <<-EOCMD
     /letsencrypt/letsencrypt-auto certonly --standalone --agree-tos $@
 EOCMD
 chmod +x "$gc"
-/bin/get_cert.sh > /var/log/le_init.log 2>&1
+
 #exec "$gc"
 exec /usr/bin/supervisord --nodaemon
