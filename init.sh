76ae9b80c27fb1a25ee28ecd934022b172eb5c72
david <david@cninone.com>
2016-04-24 09:44:10 +0800

    mod init script

--- init.sh
+++ init.sh
@@ -12,8 +12,8 @@ cat > "$gc" <<-EOCMD
 /letsencrypt/letsencrypt-auto certonly --standalone --agree-tos --standalone-supported-challenges http-01 $@
 EOCMD
 chmod +x "$gc"
-#exec "$gc"
-exec /usr/bin/supervisord --nodaemon
+exec "$gc" > /var/log/lep/init.log 2>&1
 else 
-	echo "please input parameters like this: --email xx@xxx.com -d xxx.com" 
+	echo "no input, just do renew cert monthly"     
 fi
+exec /usr/bin/supervisord --nodaemon
